d8dbeb0d616fcd5ac4ea126f50bba8f37aac85f3
Felipe Gómez <72258943+alu0101315713@users.noreply.github.com>
2023-01-16 22:13:16 +0000

    nueva interfaz con productos

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,51 +1,73 @@
 import React, { useState } from "react";
-import './App.css';
-import { Login } from "./components/Login";
-import { Register } from "./components/Register";
-import { Logeado } from "./components/Logeado"
+import './index.css';
+
+// import './App.css';
+// import { Login } from "./components/Login";
+// import { Register } from "./components/Register";
+// import { Logeado } from "./components/Logeado"
+import { Header } from "./components/Header";
+import { Carrito } from "./components/Carrito";
+import {DataProvider} from './context/DataProvider';
+import { BrowserRouter as Router} from "react-router-dom";
+import Pages from "./components/Page.js";
+import "boxicons";
 
 function App() {
 
-  const [currentForm, setCurrentForm] = useState('login');
-
-  const toggleForm = (formName) => {
-    setCurrentForm(formName);
-  }
 
   return (
-    <div>
-    <header>
-      <ul>
-        <li><a 
-          href="#"
-          role="button"
-          onClick={() => setCurrentForm('login')}>
-          Iniciar Sesión
-        </a></li>
-        <li><a 
-          href="#"
-          role="button"
-          onClick={() => setCurrentForm('signup')}>
-          Registrarse
-        </a></li>
-      </ul>
-    </header>
+    <DataProvider>
     <div className="App">
-      {
-        {
-          'login': <Login onFormSwitch={toggleForm} />,
-          'signup': <Register onFormSwitch={toggleForm} />,
-          'logeado': <Logeado onFormSwitch={toggleForm} />
-        }[currentForm]
-      }
+      <Router>
+      <Header />
+      <Carrito />
+      <Pages />
+      </Router>
     </div>
-    <footer className="footer">
-      <p className="text-footer">
-        Footer
-      </p>
-    </footer>
-    </div>
+    </DataProvider>
   );
+
+
+//   const [currentForm, setCurrentForm] = useState('login');
+
+//   const toggleForm = (formName) => {
+//     setCurrentForm(formName);
+//   }
+
+//   return (
+//     <div>
+//     <header>
+//       <ul>
+//         <li><a 
+//           href="#"
+//           role="button"
+//           onClick={() => setCurrentForm('login')}>
+//           Iniciar Sesión
+//         </a></li>
+//         <li><a 
+//           href="#"
+//           role="button"
+//           onClick={() => setCurrentForm('signup')}>
+//           Registrarse
+//         </a></li>
+//       </ul>
+//     </header>
+//     <div className="App">
+//       {
+//         {
+//           'login': <Login onFormSwitch={toggleForm} />,
+//           'signup': <Register onFormSwitch={toggleForm} />,
+//           'logeado': <Logeado onFormSwitch={toggleForm} />
+//         }[currentForm]
+//       }
+//     </div>
+//     <footer className="footer">
+//       <p className="text-footer">
+//         Footer
+//       </p>
+//     </footer>
+//     </div>
+//   );
 }
 
 export default App;
---
+++ frontend/src/Data2.js
@@ -0,0 +1,113 @@
+const data = {
+  status: "success",
+  items: [
+    {
+      id: 1,
+      title: "Dopamina",
+      price: 19,
+      image: "https://cdn.agapea.com/Ediciones-Peninsula/Dopamina-i1n21049487.jpg",
+      category: "Ciencia",
+      // img1: "https://stockx-360.imgix.net/Nike-LD-Waffle-Sacai-Black-Nylon/Images/Nike-LD-Waffle-Sacai-Black-Nylon/Lv2/img",
+      // img2: ".jpg?auto=format,compress&q=90&updated_at=1606321430&w=1000",
+      cantidad: 1,
+    },
+    {
+      id: 2,
+      title: "Hábitos atómicos",
+      price: 17.5,
+      image: "https://cdn.agapea.com/Diana-Editorial/Habitos-atomicos-i1n19380188.jpg",
+      category: "Ciencia",
+      // img1: "https://stockx-360.imgix.net/Nike-Dunk-Low-Off-White-Pine-Green/Images/Nike-Dunk-Low-Off-White-Pine-Green/Lv2/img",
+      // img2: ".jpg?auto=format,compress&q=90&updated_at=1606319825&w=1000",
+      cantidad: 1
+    },
+    {
+      id: 3,
+      title: "En la sombra",
+      price: 21.5,
+      image: "https://cdn.agapea.com/PLAZA-JANES/En-la-sombra-i1n22745851.jpg",
+      category: "Literatura",
+      // img1: "https://stockx-360.imgix.net/Nike-Air-Force-1-Low-Supreme-Box-Logo-Black/Images/Nike-Air-Force-1-Low-Supreme-Box-Logo-Black/Lv2/img",
+      // img2: ".jpg?auto=format,compress&q=90&updated_at=1606325289&w=1000",
+      cantidad: 1
+    },
+    {
+      id: 4,
+      title: "Lejos de Luisiana",
+      price: 22.5,
+      image: "https://cdn.agapea.com/Editorial-Planeta/Lejos-de-Luisiana-i1n22725249.jpg",
+      category: "Literatura",
+      // img1: "https://stockx-360.imgix.net/Nike-LD-Waffle-Sacai-White-Nylon/Images/Nike-LD-Waffle-Sacai-White-Nylon/Lv2/img",
+      // img2: ".jpg?auto=format,compress&q=90&updated_at=1606325338&w=1000",
+      cantidad: 1
+    },
+    {
+      id: 5,
+      title: "Don Quijote de la Mancha",
+      price: 4,
+      image: "https://cdn.agapea.com/Susaeta-Ediciones/Don-Quijote-de-la-Mancha-i1n13470550.jpg",
+      category: "Infantil",
+      // img1: " https://stockx-360.imgix.net/Nike-Dunk-Low-SP-Kentucky/Images/Nike-Dunk-Low-SP-Kentucky/Lv2/img",
+      // img2: ".jpg?auto=format,compress&q=90&updated_at=1606322330&w=1000",
+      cantidad: 1
+    },
+    {
+      id: 6,
+      title: "Super Geografía",
+      price: 16,
+      image: "https://cdn.agapea.com/ALFAGUARA-INFANTILES-Y-JUVENILES/Super-Geografia-i1n21593688.jpg",
+      category: "Infantil",
+      // img1: "https://stockx-360.imgix.net/Nike-Dunk-Low-Off-White-University-Red/Images/Nike-Dunk-Low-Off-White-University-Red/Lv2/img",
+      // img2: ".jpg?auto=format,compress&q=90&updated_at=1606321824&w=1000",
+      cantidad: 1
+    },
+    {
+      id: 7,
+      title: "Tengo un volcán",
+      price: 11.5,
+      image: "https://cdn.agapea.com/Carambuco-Ediciones/Tengo-un-volcan-i1n17425199.jpg",
+      category: "Infantil",
+      // img1: "https://stockx-360.imgix.net/Nike-Air-Max-2-Light-Atmos/Images/Nike-Air-Max-2-Light-Atmos/Lv2/img",
+      // img2: ".jpg?auto=format,compress&q=90&updated_at=1606320966&w=1000",
+      cantidad: 1
+    },
+    {
+      id: 8,
+      title: "El libro de los animales",
+      price: 12.5,
+      image: "https://cdn.agapea.com/Editorial-Planeta/El-libro-de-los-animales-i1n22725250.jpg",
+      category: "Infantil",
+      // img1: "https://stockx-360.imgix.net/Nike-Air-Max-2-Light-Atmos/Images/Nike-Air-Max-2-Light-Atmos/Lv2/img",
+      // img2: ".jpg?auto=format,compress&q=90&updated_at=1606320966&w=1000",
+      cantidad: 1
+    },
+    // otro libro de literatura
+    {
+      id: 9,
+      title: "El ritual",
+      price: 18,
+      image: "https://cdn.agapea.com/Minotauro/El-ritual-NE--i1n23251005.jpg",
+      category: "Terror",
+      cantidad: 1
+    },
+    {
+      id: 10,
+      title: "El hombre que amaba a los perros",
+      price: 18,
+      image: "https://cdn.agapea.com/Minotauro/El-hombre-que-amaba-a-los-perros-i1n23251006.jpg",
+      category: "Literatura",
+      cantidad: 1
+    },
+    // otro de terror
+    {
+      id: 11,
+      title: "El hombre que amaba a los perros",
+      price: 16.5,
+      image: "https://cdn.agapea.com/Punto-de-Lectura/It-i1n15714279.jpg",
+      category: "Terror",
+      cantidad: 1
+    },
+  ],
+};
+
+export default data;
