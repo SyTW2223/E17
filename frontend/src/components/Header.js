7eedf6c234cc3a49e8a3b33921798f7cb4f33e62
Noelia Ibáñez <alu0101225555@ull.edu.es>
2023-01-19 13:23:34 +0100

    pruebas header no funcional

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -2,7 +2,8 @@ import React, { useContext} from "react";
 import { DataContext } from "../context/DataProvider";
 import { Link } from "react-router-dom";
 import Icono from "../images/icono.jpg";
-
+//import { Login } from "../components/Login.js";
+//import { useState } from "react";
 
 export const Header = () => {
   const value = useContext(DataContext);
@@ -13,7 +14,6 @@ export const Header = () => {
   const toogleMenu = () =>{
     setMenu(!menu)
   }
- 
 
   return (
     <header>
@@ -37,6 +37,17 @@ export const Header = () => {
         <box-icon name="cart"></box-icon>
         <span className="item__total"> {carrito.length} </span>
       </div>
+      <li>
+        <Link to="/login"><box-icon name="user"></box-icon></Link>
+      </li>
     </header>
   );
 };
+
+
+
+
+{/* <Link to="frontend\src\components\Login.jsx"><box-icon name="user"></box-icon></Link> */}
+{/* <a href="Login"><box-icon name="user"></box-icon> Login</a> */}
+{/* <li><a href="./Login" role="button" onClinck={() => setCurrentForm('login')}> Login</a></li> */}
+{/* <Link to="../"><box-icon name="user"></box-icon></Link> */}
---
+++ frontend/src/components/page/login/index.js
@@ -0,0 +1,58 @@
+/* import React, { useState } from "react";
+import './index.css';
+
+// import './App.css';
+// import { Login } from "./components/Login";
+import { Register } from "./components/Register";
+import { Logeado } from "./components/Logeado"
+import { Header } from "./components/Header";
+import { Carrito } from "./components/Carrito";
+import {DataProvider} from './context/DataProvider';
+import { BrowserRouter as Router} from "react-router-dom";
+import Pages from "./components/Page.js";
+import Footer from "./pages/global/Footer"
+import "boxicons";
+
+function Login() {
+
+    const [currentForm, setCurrentForm] = useState('login');
+
+   const toggleForm = (formName) => {
+     setCurrentForm(formName);
+   }
+
+    return (
+     <div>
+     <header>
+       <ul>
+         <li><a 
+           href="#"
+           role="button"
+           onClick={() => setCurrentForm('login')}>
+           Iniciar Sesión
+         </a></li>
+         <li><a 
+           href="#"
+           role="button"
+           onClick={() => setCurrentForm('signup')}>
+           Registrarse
+         </a></li>
+       </ul>
+     </header>
+     <div className="App">
+       {
+         {
+           'login': <Login onFormSwitch={toggleForm} />,
+           'signup': <Register onFormSwitch={toggleForm} />,
+           'logeado': <Logeado onFormSwitch={toggleForm} />
+         }[currentForm]
+       }
+     </div>
+     <footer className="footer">
+       <p className="text-footer">
+         Footer
+       </p>
+     </footer>
+     </div>
+   );
+} */
